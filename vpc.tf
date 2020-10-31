dc0ecc41065f6c16be42a08d0978983ba812d84a
Pat <psaneem@gmail.com>
2020-10-31 16:28:01 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,6 @@ als {
   }
 
   local_vpc_name         = "aws-${var.region}-${local.common_tags["Vertical"]}-${local.common_tags["Application"]}-${local.common_tags["Environment"]}"
-  #cloudwatch_destination = "arn:aws:logs:${var.region}:348426448370:destination:sec_flowlogs_cwd"
 }
 
 #####
@@ -15,21 +14,9 @@ als {
 #####
 data "aws_caller_identity" "current" {}
 
-#data "aws_iam_account_alias" "current" {}
-
-#data "aws_ec2_transit_gateway" "main" {}
 
 data "aws_availability_zones" "azs" {}
 
-#data "template_file" "checkpoint_config" {
-#  template = "${file("${path.module}/checkpoint_config.tpl")}"
-
-#  vars {
-#    vpc_name    = "${local.local_vpc_name}"
-#    vpc_netaddr = "${cidrhost(aws_vpc.main.cidr_block,0)}"
-#    vpc_netmask = "${cidrnetmask(aws_vpc.main.cidr_block)}"
-#  }
-#}
 
 #####
 #Basic VPC Infrastructure
