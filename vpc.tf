e310bf0e4d1ce553425a5a2125ec52894fcc31bd
Pat <psaneem@gmail.com>
2020-11-01 16:08:17 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,7 @@ locals {
     "ProvisionedBy" = "terraform"
   }
 
-  local_vpc_name         = "aws-${var.region}-${local.common_tags["Vertical"]}-${local.common_tags["Application"]}-${local.common_tags["Environment"]}"
+  #local_vpc_name         = "aws-${var.region}-${local.common_tags["Vertical"]}-${local.common_tags["Application"]}-${local.common_tags["Environment"]}"
 }
 
 #####
@@ -24,16 +24,4 @@ data "aws_availability_zones" "azs" {}
 
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_supernet
-  enable_dns_hostnames = var.enable_public_dns_hostnames && var.enable_vpc_dns ? "true" : "false"
-  enable_dns_support   = var.enable_vpc_dns ? "true" : "false"
-
-  tags = "${merge(
-    local.common_tags,
-    map(
-      "Name", local.local_vpc_name",
-      "DataClassification", "confidential",
-      "PubliclyAccessible", "false",
-      "app_tag",var.app_tag"
-      )
-      )}"
 }
