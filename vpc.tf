27b0336113bb152e7e168945c305b673067ab26c
Pat <psaneem@gmail.com>
2020-11-01 16:09:47 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,26 +1,3 @@
-locals {
-  common_tags {
-    Vertical      = var.tag_vertical
-    Application   = "var.tag_application
-    Environment   = var.tag_environment
-    "ProvisionedBy" = "terraform"
-  }
-
-  #local_vpc_name         = "aws-${var.region}-${local.common_tags["Vertical"]}-${local.common_tags["Application"]}-${local.common_tags["Environment"]}"
-}
-
-#####
-#Data Sources
-#####
-data "aws_caller_identity" "current" {}
-
-
-data "aws_availability_zones" "azs" {}
-
-
-#####
-#Basic VPC Infrastructure
-#####
 
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_supernet
