3dea3c6be54498585d16ee15bf68783e6c4358b7
Pat <psaneem@gmail.com>
2020-11-09 12:06:05 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,13 @@
+terraform {
+    backend "remote" {
+        hostname = "app.terraform.io"
+        organization = "healtpharm"
 
+        workspaces {
+            name = "acme"
+        }
+    }
+}
 resource "aws_vpc" "main" {
   cidr_block           = var.cidr
 }
