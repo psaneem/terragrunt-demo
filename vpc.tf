063043569b435596574a96d36b175d499427b758
Pat <psaneem@gmail.com>
2020-11-09 12:07:24 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -8,6 +8,13 @@ terraform {
         }
     }
 }
+
+provider "aws" {
+    #region = "${local.aws_region}"
+    region = "us-east-1"
+    }
+
+
 resource "aws_vpc" "main" {
   cidr_block           = var.cidr
 }
